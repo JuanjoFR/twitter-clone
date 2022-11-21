7291794feb88e719cbecf8af7b7751169fb4e408
Juanjo Fernández <juanjo.fernandez@haa.st>
2022-11-21 11:52:54 +0100

    Create website and add pattern library module

---
+++ website/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "pattern-library/dist/build.css"
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
