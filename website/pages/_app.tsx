2c86f054568fe006c0efc695b8a03245af671f9b
Juanjo Fernández <juanjo.fernandez@haa.st>
2022-12-27 17:15:00 +0100

    Add lint to website

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "pattern-library/dist/build.css"
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
